5abb963c4b48a0794422fd72bb75ea50703217ca
Noe <guevaranoeliaa@gmail.com>
2023-12-21 00:28:45 -0300

    funciones, navbar oradores, validaciones, ajustes

--- JS/main.js
+++ JS/main.js
@@ -1,49 +1,8 @@
-import { calcular_descuentos } from "./modules/funciones.js";
+import { calcular_descuentos } from "./modules/funciones.js"
 import { validarDatos } from "./modules/funciones.js";
 import { agregarResultado } from "./modules/funciones.js";
 
 
-
-
-function enviar(){
-        //debugger;
-        let nombre = document.getElementById('campo_nombre').value;
-        let apellido = document.getElementById('campo_apellido').value;
-        let mail = document.getElementById('campo_email').value;
-        let tema = document.getElementById('mje').value;
-
-        
-        const orador = {
-            nombre,
-            apellido,
-            mail,
-            tema
-        };
-        
-
-
-        alert("Tu mensaje ha sido enviado " + nombre + " " + apellido + "!");
-        //post al servidor
-        //1 preparo la peticion
-        const respuesta = fetch(`http://localhost:8080/web-app/api/orador`, {
-            method: 'POST',
-            body: JSON.stringify(orador)
-        });
-
-        //2 intento reosolver la promesa
-        respuesta
-            .then(response => response.json())
-            .then(respuesta => {
-                //actualizar el div del html con la informacion
-                alert(`Se ha dado de alta el orador id: ${respuesta.id}`);
-                listarOradores();
-                limpiarInputOradores();
-            })
-            .catch(error => console.log(error))
-
-    }
-
-   
 //Linkeo el botón que voy a usar
 
 const button = document.getElementById('resumen1');
@@ -70,14 +29,13 @@ function resumen() {
 
         agregarResultado(resultado);
 
-} else {
-    alert("Verifique los datos ingresados");
-}
+    } else {
+        alert("Verifique los datos ingresados");
+    }
 }
 
 
-window.enviar = enviar;
 window.resumen = resumen;
 
-//Funciones Backend
+
 
--- JS/modules/funciones.js
+++ JS/modules/funciones.js
@@ -24,17 +24,14 @@ function calcular_descuentos(cantidad,precio,categoria){
     }; 
 };
 
+
 //Creo función que valide datos de compra ingresados
-
-
-
 
 function validarDatos (cantidad, categoria){
 
     return cantidad > 0 && categoria != "seleccione";
 
 } 
-
 
 
 //Hago función para pasar el resultado al html
@@ -44,12 +41,239 @@ function agregarResultado(resultado) {
     let parrafo = document.getElementById("totalAPagar");
    
     parrafo.innerHTML = `<span class="montoTotal">Total a pagar $ ${resultado}</span>`;
+}
 
+
+//Funciones Orador
+
+    let oradorId;
+    let oradores = [];
+    let oradorActual;
+
+    const setId = (id) => {
+        oradorId = id;
+        const orador = oradores.find(o => o.id === id);
+        oradorActual = orador;
+        
+
+        document.getElementById('nombreActualizar').value = oradorActual.nombre;
+        document.getElementById('apellidoActualizar').value = oradorActual.apellido;
+        document.getElementById('mailActualizar').value = oradorActual.mail;
+        document.getElementById('temaActualizar').value = oradorActual.tema;
+    }
+
+    const setOradores = (nuevosOradores) => {
+        oradores = nuevosOradores;
+    }
+
+    const actualizarOrador = () => {
+        //debugger;
+        if(!oradorActual) {
+            return;
+        }
+        const nombre = document.getElementById('nombreActualizar').value;
+        const apellido = document.getElementById('apellidoActualizar').value;
+        const mail = document.getElementById('mailActualizar').value;
+        const tema = document.getElementById('temaActualizar').value;
+                
+        const orador = {
+            nombre,
+            apellido,
+            mail,
+            tema
+        };
+
+        //put al servidor
+        //1 preparo la peticion
+        const respuesta = fetch(`http://localhost:8080/web-app/api/orador?id=${oradorActual.id}`,{
+            method: 'PUT',
+            body: JSON.stringify(orador)
+        });
+
+        //2 intento reosolver la promesa
+        respuesta
+            .then(response => response)//solo recibe la confirmacion de ok y con eso lanza el alert
+            .then(respuesta => {
+                //debugger;
+                //actualizar el div del html con la informacion
+                alert(`Se ha actualizado el orador id: ${oradorActual.id}`);
+
+                //cerrar el modal
+                document.getElementById('btnCerrar').click()
+                listarOradores();
+            })
+            .catch(error => console.log(error))
+    }
+
+    try {
+        document.getElementById('btnActualizar').addEventListener('click', actualizarOrador);
+        document.getElementById('btnListado').addEventListener('click', listarOradores);
+    } catch { }
+    
+
+
+    const nuevoOrador =(nombreId, apellidoId, emailId, temaId) => { 
+        // debugger;
+         const nombre = document.getElementById(nombreId).value;
+         const apellido = document.getElementById(apellidoId).value;
+         const mail = document.getElementById(emailId).value;
+         const tema = document.getElementById(temaId).value;
+     
+         const orador = {
+             nombre,
+             apellido,
+             mail,
+             tema
+         };
+     
+         //post al servidor
+         //1 preparo la peticion
+         const respuesta = fetch('http://localhost:8080/web-app/api/orador',{
+             method: 'POST',
+             body: JSON.stringify(orador)
+         });
+     
+         //2 intento reosolver la promesa
+         respuesta
+             .then(response => response.json())
+             .then(respuesta => {
+                 //actualizar el div del html con la informacion
+                 alert(`Se ha dado de alta el orador id ${respuesta.id}: ${respuesta.nombre} ${respuesta.apellido}`);
+             })
+             .catch(error => console.log(alert('El email ya se encuentra registrado') + error), )
+     }
+     
+     
+
+const eliminarOrador = (id) => {
+   // debugger;
+    if (!confirm(`Esta seguro de eliminar el registro: ${id} ?`)) { 
+        return;
+    }
+    const respuesta = fetch(`http://localhost:8080/web-app/api/orador?id=${id}`, {
+        method: 'DELETE',
+    });
+
+    //2 intento reosolver la promesa
+    respuesta
+        .then(response => response)
+        .then(respuesta => {
+            //actualizar el div del html con la informacion
+            alert(`Se ha eliminado el orador id: ${id}`);
+            listarOradores();
+        })
+        .catch(error => console.log(error))}
+
+// const nuevoOrador = () => { 
+//     //debugger;
+//     const nombre = document.getElementById('nombre').value;
+//     const apellido = document.getElementById('apellido').value;
+//     const mail = document.getElementById('mail').value;
+//     const tema = document.getElementById('tema').value;
+
+//     const orador = {
+//         nombre,
+//         apellido,
+//         mail,
+//         tema
+//     };
+
+//     //post al servidor
+//     //1 preparo la peticion
+//     const respuesta = fetch(`http://localhost:8080/web-app/api/orador`, {
+//         method: 'POST',
+//         body: JSON.stringify(orador)
+//     });
+
+//     //2 intento reosolver la promesa
+//     respuesta
+//         .then(response => response.json())
+//         .then(respuesta => {
+//             //actualizar el div del html con la informacion
+//             alert(`Se ha dado de alta el orador id: ${respuesta.id}`);
+//             listarOradores();
+           
+//         })
+//         .catch(error => console.log(error))
+
+// }
+// document.getElementById('btnGrabar').addEventListener('click', nuevoOrador);
+
+
+
+function listarOradores() {
+
+    //1 preparo la peticion
+    const respuesta = fetch('http://localhost:8080/web-app/api/orador');
+    
+    //2 intento reosolver la promesa
+    respuesta
+        .then(response => response.json())
+        .then(users => {
+            //actualizar el div del html con la informacion
+            dibujarTabla(users)
+            setOradores(users);
+        })  
+        .catch(error => console.log(error))
+}
+
+function dibujarTabla(data) {
+    const rows = dibujarFilas(data);
+
+    try { document.getElementById('usersRows').innerHTML = rows; }
+
+    catch { }   
+    
+}
+
+function dibujarFilas(oradores) {
+    let rows = ``;
+    for (let orador of oradores) {//ctrl+d ctr+f2
+        //console.log(user)
+        rows += `
+    <tr>
+        <th scope="row">${orador.id}</th>
+        <td>${orador.nombre}</td>
+        <td>${orador.apellido}</td>
+        <td>${orador.tema}</td>
+        <td>${orador.mail}</td>
+        <td>
+            <button onClick="eliminarOrador(${orador.id})"><i class="fas fa-trash"></i></button>
+            <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick="setId(${orador.id})">Editar</button>
+        </td>
+        
+    </tr>
+    `
+    }
+    return rows;
 }
 
 
 
+
+
+// function limpiarInputOradores(){
+//     nombre.value="";
+//     apellido.value="";
+//     mail.value="";
+//     tema.value="";
+// }
+
+
+
+
+
+
+//Evento automático
+window.addEventListener("load", (event) => {
+    listarOradores();
+});
+
 export {calcular_descuentos, validarDatos, agregarResultado};
 
 window.borrar = borrar;
+window.setId= setId;
+window.nuevoOrador = nuevoOrador;
+window.eliminarOrador= eliminarOrador;
+
 
