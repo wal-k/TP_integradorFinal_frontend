79c552591798165c748b5fa8f47d9881fed7e11e
Noe <guevaranoeliaa@gmail.com>
2023-12-21 00:37:24 -0300

    Se elimina link duplicado

--- JS/modules/funciones.js
+++ JS/modules/funciones.js
@@ -164,41 +164,6 @@ const eliminarOrador = (id) => {
         })
         .catch(error => console.log(error))}
 
-// const nuevoOrador = () => { 
-//     //debugger;
-//     const nombre = document.getElementById('nombre').value;
-//     const apellido = document.getElementById('apellido').value;
-//     const mail = document.getElementById('mail').value;
-//     const tema = document.getElementById('tema').value;
-
-//     const orador = {
-//         nombre,
-//         apellido,
-//         mail,
-//         tema
-//     };
-
-//     //post al servidor
-//     //1 preparo la peticion
-//     const respuesta = fetch(`http://localhost:8080/web-app/api/orador`, {
-//         method: 'POST',
-//         body: JSON.stringify(orador)
-//     });
-
-//     //2 intento reosolver la promesa
-//     respuesta
-//         .then(response => response.json())
-//         .then(respuesta => {
-//             //actualizar el div del html con la informacion
-//             alert(`Se ha dado de alta el orador id: ${respuesta.id}`);
-//             listarOradores();
-           
-//         })
-//         .catch(error => console.log(error))
-
-// }
-// document.getElementById('btnGrabar').addEventListener('click', nuevoOrador);
-
 
 
 function listarOradores() {
@@ -250,17 +215,12 @@ function dibujarFilas(oradores) {
 
 
 
-
-
 // function limpiarInputOradores(){
 //     nombre.value="";
 //     apellido.value="";
 //     mail.value="";
 //     tema.value="";
 // }
-
-
-
 
 
 
